4b6ebed225a963e1f92f8d64e4ee668e7f375d00
Freshnuts <fr3shnuts@gmail.com>
2021-09-08 18:11:20 -0700

    updated

--- start.sh
+++ start.sh
@@ -3,23 +3,44 @@
 
 
 # Update & Upgrade
-sudo apt update
-sudo -y apt 
+sudo apt -y update dist-upgrade
 
 # NVIDIA drivers
 nvidia-detector
+sudo apt -y install nvidia-driver-470
 
+# Python Installation
+sudo apt install -y python2 python3 python3-dev python3-pip
 
-# Installations
-sudo apt install vim vim-youcompleteme gcc-multilib hddtemp clamav gdb gdbserver
+# Virtual Environments Installations
+sudo apt -y install virtualbox qemu qemu-system-arm qemu-system-mips docker
+
+# Pwn Environment Installations
+sudo apt -y install gdb gdbserver gcc-multilib ltrace strace valgrind gcc-multilib
+
+sudo wget 'https://bootstrap.pypa.io/pip/2.7/get-pip.py' && sudo python2 get-pip.py
+sudo apt-get -y install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade pwntools
+sudo apt-get -y install python python-dev git libssl-dev libffi-dev build-essential
+sudo python2 -m pip install --upgrade pwntools
+
+# AV & Security Installations
+sudo apt -y install clamav openvas
+
+# IDE
+sudo apt -y install vim vim-youcompleteme
+
+# Misc Installations
+sudo apt -y install hddtemp hwinfo vlc evince eog
+
+# RF Software
+sudo apt -y install gnuradio gqrx-sdr
 
 # Removals
-sudo apt remove mintupdate
-sudo apt remove
-
+sudo apt -y remove mintupdate
 
 # Fix Popping sound issue @ /etc/pulse/default.pa
-sudo su
-sed -i "s/load-module module-suspend-on-idle/#load-module module-suspend-on-idle/g" /etc/pulse/default.pa
+sudo sed -i "s/load-module module-suspend-on-idle/#load-module module-suspend-on-idle/g" /etc/pulse/default.pa
 exit
 
