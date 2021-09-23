2485e0969c95648f057dad45c976b47e0a35f1da
Freshnuts <fr3shnuts@gmail.com>
2021-09-23 16:48:10 -0700

    updated - bluetooth

--- start.sh
+++ start.sh
@@ -24,6 +24,21 @@ else
         
 fi
 
+# Bluetooth - my earbuds
+# coproc allows input to another process and can relays output.
+coproc bluetoothctl
+echo -e 'show\nscan on\n'  >&${COPROC[1]}
+sleep 3
+echo -e 'exit\n' >&${COPROC[1]}
+
+output=$(cat <&${COPROC[0]})
+echo $output
+
+tozo=$(bluetoothctl devices | grep -i 'tozo-t6' | cut -d' ' -f2)
+bluetoothctl pair $tozo
+bluetoothctl connect $tozo
+
+
 # Python Installation
 sudo apt install -y python2 python3 python3-dev python3-pip
 
