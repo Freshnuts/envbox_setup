4ed33f682a135c395cd5daffcbe0d32f9b4168c0
Freshnuts <fr3shnuts@gmail.com>
2021-09-23 17:16:53 -0700

    remove device update

--- bluetooth_connect.sh
+++ bluetooth_connect.sh
@@ -10,20 +10,23 @@ read answer
 if [ $answer == 1 ]
 then
 	coproc bluetoothctl
-	echo -e 'show\nscan on\n'  >&${COPROC[1]}
+	echo -e 'scan on\n'  >&${COPROC[1]}
 	sleep 3
 	echo -e 'exit\n' >&${COPROC[1]}
-	output=$(cat <&${COPROC[0]})
-	echo $output
+
+	# output=$(cat <&${COPROC[0]})
+	# echo $output
+
 	tozo=$(bluetoothctl devices | grep -i 'tozo-t6' | cut -d' ' -f2)
 	bluetoothctl pair $tozo
 	bluetoothctl connect $tozo
 
 elif [ $answer == 2 ]
 then
-	bluetoothctl disconnect $tozo
+	tozo=$(bluetoothctl devices | grep -i 'tozo-t6' | cut -d' ' -f2)
+	bluetoothctl remove $tozo
 
 else
 	echo -e 'Error\n'
-  
 fi
+
