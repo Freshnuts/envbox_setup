8264d28b7c7c8b8a98a80872409012492216fcbe
Freshnuts <fr3shnuts@gmail.com>
2021-09-23 17:09:02 -0700

    Added functionality

--- bluetooth_connect.sh
+++ bluetooth_connect.sh
@@ -2,14 +2,28 @@
 
 # Bluetooth - my earbuds
 # coproc allows input to another process and can relays output.
-coproc bluetoothctl
-echo -e 'show\nscan on\n'  >&${COPROC[1]}
-sleep 3
-echo -e 'exit\n' >&${COPROC[1]}
 
-output=$(cat <&${COPROC[0]})
-echo $output
+echo -e "\n1: Connect or 2: Disconnect"
 
-tozo=$(bluetoothctl devices | grep -i 'tozo-t6' | cut -d' ' -f2)
-bluetoothctl pair $tozo
-bluetoothctl connect $tozo
+read answer
+
+if [ $answer == 1 ]
+then
+	coproc bluetoothctl
+	echo -e 'show\nscan on\n'  >&${COPROC[1]}
+	sleep 3
+	echo -e 'exit\n' >&${COPROC[1]}
+	output=$(cat <&${COPROC[0]})
+	echo $output
+	tozo=$(bluetoothctl devices | grep -i 'tozo-t6' | cut -d' ' -f2)
+	bluetoothctl pair $tozo
+	bluetoothctl connect $tozo
+
+elif [ $answer == 2 ]
+then
+	bluetoothctl disconnect $tozo
+
+else
+	echo -e 'Error\n'
+  
+fi
