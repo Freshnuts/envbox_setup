c6b6b246809a7742c71485501b9506a52f3eb208
Freshnuts <fr3shnuts@gmail.com>
2021-10-05 20:36:10 -0700

    Updated Menu

--- bluetooth_connect.sh
+++ bluetooth_connect.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+
 # Bluetooth - my earbuds
-# coproc allows input to another process and can relays output.
+# coproc allows interprocess communication.
 
 bluetooth_device=$1
 
+
 if [ $# != 1 ]
 then
+	echo
+	echo -e 'Bluetooth Connector'
 	echo -e 'Usage: blue <device name>\n'
 	exit
 fi
 
-echo -e "\nBluetooth Earbuds."
-echo -e "\n1: Connect\n2: Disconnect"
-
+echo -e "\nBluetooth Connector."
+echo -e "\n1. Connect\n2. Disconnect\n3. Exit"
+echo -en ">: "
 read answer
 
 if [ $answer == 1 ]
 then
+
+
+
+
 	coproc bluetoothctl
+	echo -e '\nPairing:...'
 	echo -e 'scan on\n'  >&${COPROC[1]}
 	sleep 3
 	echo -e 'exit\n' >&${COPROC[1]}
@@ -26,15 +35,17 @@ then
 	# output=$(cat <&${COPROC[0]})
 	# echo $output
 
-	
 	bt_search=$(bluetoothctl devices | grep -i "$1" | cut -d' ' -f2)
 	bluetoothctl pair $bt_search
 	bluetoothctl connect $bt_search
 
 elif [ $answer == 2 ]
 then
-	$bt_search	
+	bt_search=$(bluetoothctl devices | grep -i "$1" | cut -d' ' -f2)
 	bluetoothctl remove $bt_search
+elif [ $answer == 3 ]
+then
+	exit
 
 else
 	echo -e 'Error\n'
