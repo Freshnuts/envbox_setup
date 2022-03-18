0205773c7f797cf7eb07f12a983a6256d34328d3
Fresh <fr3shnuts@gmail.com>
2022-03-17 20:36:47 -0700

    'added hping3'

--- start.sh
+++ start.sh
@@ -61,7 +61,7 @@ sudo apt -y install hddtemp hwinfo vlc evince eog tmux
 sudo apt -y install scalpel foremost testdisk autopsy
 
 # Networking
-sudo apt -y install nmap masscan wireshark tcpdump iptables snort ufw screen
+sudo apt -y install nmap masscan wireshark tcpdump iptables snort ufw screen hping3
 sudo git clone 'https://github.com/rbsec/dnscan.git' /opt/dnscan
 sudo git clone 'https://github.com/lanmaster53/recon-ng.git' /opt
 
